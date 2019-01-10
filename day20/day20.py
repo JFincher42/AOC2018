20d29c8e14c1badc9d6d3145df460db95f16af52
Jon Fincher <jfincher42@gmail.com>
2019-01-10 15:59:44 -0600

    Part 1 and part 2 complete.

--- day20/day20.py
+++ day20/day20.py
@@ -2,24 +2,31 @@
 AoC 2018 Day 20
 '''
 import numpy as np
+import sys
 
 # CONSTANTS
-NORTH = 1
-WEST  = 2
-SOUTH = 4
-EAST  = 8
+NORTH   = 1
+WEST    = 2
+SOUTH   = 4
+EAST    = 8
+VISITED = 16
 
 def part1():
+    # The map is a global so everyone can access it easily
     global map
+
+    # As we go along, we may need to add rows or columns to the map
+    global add_rows, add_cols
+
     # Input File
-    # filename = "./day20/input.txt"
+    filename = "./day20/input.txt"
 
     # Sample Input
     # filename = "./day20/sample1.txt"
     # filename = "./day20/sample2.txt"
     # filename = "./day20/sample3.txt"
     # filename = "./day20/sample4.txt"
-    filename = "./day20/sample5.txt"
+    # filename = "./day20/sample5.txt"
 
     # Setup a global numpy array to track our position
     # We encode open doors in bytes
@@ -28,7 +35,7 @@ def part1():
     # -           4: South
     #
     # We assume all doors marked zero are closed
-    map = np.zeros([10,10], dtype=np.int64)
+    map = np.zeros([100,100], dtype=np.int64)
     
     # Start in the middle
     # We can add items as necessary to grow the array
@@ -36,7 +43,8 @@ def part1():
     # Add to the SOUTH: map = np.insert(map, map.shape[0], [0], 0)
     # Add to the EAST:  map = np.insert(map, map.shape[1], [0], 1)
     # Add to the WEST:  map = np.insert(map,           0 , [0], 1)
-    cx, cy = (5, 5)
+    cx, cy = 48, 48
+    add_rows, add_cols = 0, 0
 
     # Read the file
     with open(filename, "r") as infile:
@@ -45,36 +53,119 @@ def part1():
         while ch != '^':
             ch = infile.read(1)
 
-        # Now read until the final char is seen
+        # Now read the char past the beginning character
         ch = infile.read(1)
-        # OK, now we do a recursive descent parser
+
+        # We are basically implementing a recursive descent parser
         # There are four types of characters we can see
-        # - The letters E, N, W, or S, indicating a directions from the current location
+        # - The letters E, N, W, or S, indicating a direction from the current location
         # - An open parentheses, indicating the start of a set of branches
         # - A closed parentheses, indicating the end of a set of branches
+        #   This is only a valid character after an opening paren
         # - A vertical bar (|), indicating the end of a branch, and the start of another
+        # 
+        # We assume the input is correct, so we're not doing any error checking
 
         parse(ch, infile, cx, cy)
 
-    # We're done, let's clean all the extra zero rows/cols the map
+    # Let's clean all the extra zero rows/cols the map
     # Adjust CX/CY as appropriate as well
+    # First clear out any extra rows/cols from the beginning
     while (np.all(map[0] == 0)):
         map = np.delete(map,0,0)
-        cy -= 1
+        add_rows -= 1
     while (np.all(map[:,0] == 0)):
         map = np.delete(map,0,1)
-        cx -= 1
+        add_cols -= 1
     while (np.all(map[-1] == 0)):
         map = np.delete(map,-1,0)
     while (np.all(map[:,-1] == 0)):
         map = np.delete(map,-1,1)
 
+    # If we added rows and columns, we need to increment cx and cy to account for them
+    cx += add_cols
+    cy += add_rows
+
     # and then print it to check
-    print(map)
-    print(f"CX = {cx}, CY = {cy}")
-            
+    # print(map)
+    # print(f"Width = {len(map[0])}")
+    # print(f"CX = {cx}, CY = {cy}")
+
+    # Now we find the longest path
+    find_longest(cx, cy, 0)
+
+    # Flatten map, find the largest number
+    print(f"Longest path is {np.amax(map) >> 8}")
+
+def part2():
+    global map
+
+    flattened_map = np.sort(map, axis=None)
+    top_paths = flattened_map[np.where(flattened_map > (1000<<8))]
+    print(f"Part 2: 1000 door paths: {len(top_paths)}")
+
+def find_unvisited_neighbors(cx, cy):
+    '''
+    Finds all the neighbors of the given coordinate which have not been visited
+    Returns a list of tuples of map coordinates of the unvisited neighbors
+    If there are none, the returned list has zero elements.
+    '''
+
+    global map
+
+    neighbors = []
+    if map[cy, cx] & NORTH and not map[cy-1, cx] & VISITED:
+        neighbors.append((cx, cy-1))
+    if map[cy, cx] & SOUTH and not map[cy+1, cx] & VISITED:
+        neighbors.append((cx, cy+1))
+
+    if map[cy, cx] & EAST and not map[cy, cx+1] & VISITED:
+        neighbors.append((cx+1, cy))
+    if map[cy, cx] & WEST and not map[cy, cx-1] & VISITED:
+        neighbors.append((cx-1, cy))
+
+    return neighbors
+
+
+def find_longest(cx, cy, path_len):
+    '''
+    Traverses the entire map and notesd how many steps it takes to get everywhere
+    Uses a variation of a longest path algorithm for acyclic digraphs, since our map 
+    is basically a digraph.
+
+    Uses iteration if there is a single path, and recursion to handle multiple paths
+    If there are no more paths, we return
+    '''
+
+    global map
+    # Starting at cx, cy we do the following:
+    # - Mark the current node as visited, and set the path_len
+    # - Find all the unvisited nodes adjacent to us
+    # - If there are none, we are done
+    # - If there is only one, move to that one and loop
+    # - If there is more than one, recursively scan each
+
+    looping = True
+    while looping:
+        map[cy, cx] |= VISITED
+        map[cy, cx] = (map[cy, cx] & 255) | (path_len << 8)
+        neighbors = find_unvisited_neighbors(cx, cy)
+        if len(neighbors) == 0:
+            looping = False
+    
+        elif len(neighbors) == 1:
+            cx, cy = neighbors[0]
+            path_len += 1
+
+        else:
+            for neighbor in neighbors:
+                cx, cy = neighbor
+                find_longest(cx, cy, path_len+1)
+            #looping = False
+
 def parse(ch, infile, cx, cy):
     global map
+    global add_rows, add_cols
     # ch is the current character
     # infile is where to get the next character
     # cx, cy is the current position on the map
@@ -95,11 +186,14 @@ def parse(ch, infile, cx, cy):
 
             # Can we move north? If not, add a row above us
             if cy == 0:
-                map = np.insert(map, 0, [0], 0)
+                map = np.insert(map, 0, 0, 0)
+                # And adjust all our y-coordinates to reflect a new row
                 cy += 1
                 save_cy += 1
+                add_rows += 1
+            # Move north
             cy -= 1
-            # Now add a south door here
+            # And add a south door here
             map[cy, cx] |= SOUTH
 
         elif ch == "S":
@@ -107,8 +201,8 @@ def parse(ch, infile, cx, cy):
             map[cy, cx] |= SOUTH
 
             # Can we move south? If not, add a row below us
-            if cy == map.shape[0]:
-                map = np.insert(map, map.shape[0], [0], 0)
+            if cy == map.shape[0]-1:
+                map = np.insert(map, map.shape[0], 0, 0)
             cy += 1
             # Now add a north door here
             map[cy, cx] |= NORTH
@@ -119,9 +213,10 @@ def parse(ch, infile, cx, cy):
 
             # Can we move west? If not, add a column to the left
             if cx == 0:
-                map = np.insert(map, 0, [0], 1)
+                map = np.insert(map, 0, 0, 1)
                 cx += 1
                 save_cx += 1
+                add_cols += 1
             cx -= 1
             # Now add an east door here
             map[cy, cx] |= EAST
@@ -131,22 +226,22 @@ def parse(ch, infile, cx, cy):
             map[cy, cx] |= EAST
 
             # Can we move east? If not, add a row to the right
-            if cx == map.shape[1]:
-                map = np.insert(map, map.shape[1], [0], 1)
+            if cx == map.shape[1]-1:
+                map = np.insert(map, map.shape[1], 0, 1)
             cx += 1
             # Now add a west door here
             map[cy, cx] |= WEST
 
         # An open paren - we recurse through this
         elif ch == "(":
+            # Read the char past the open paren, and recurse
             ch = infile.read(1)
             parse(ch, infile, cx, cy)
 
-        # A branch char - we recurse, but from our saved position
+        # A branch char, so continue from our saved position
         elif ch == "|":
             # Reset CX/CY to the saved values
-            cx, cy = save_cx, save_cy
-            # parse(ch, infile, save_cx, save_cy)
+            cx, cy = (save_cx, save_cy)
 
         # Something else - just return
         else:
@@ -157,3 +252,4 @@ def parse(ch, infile, cx, cy):
 
 if __name__ == "__main__":
     part1()
+    part2()
